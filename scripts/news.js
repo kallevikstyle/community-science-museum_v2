365b83e10bc882ea7bd71ab632e036144c93478d
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-11-19 14:07:44 +0100

    Loading news from JSON on index and news.html

--- scripts/news.js
+++ scripts/news.js
@@ -1,22 +1,46 @@
 function getNews(news) {
-	// Iterate through items in JSON file
-	for (let i = 0; i < news.length; i++) {
-		// Display all found items
-		displayNews(news[i]);
-	} 
+	// Sort news items by date
+		news = news.sort(function(a, b) {
+			a.date = new Date(a.date);
+			b.date = new Date(b.date);
+			return a.date - b.date;
+		});
+	// Check if all news articles will be displayed, or just the latest items
+	if (document.querySelector('.allNews')) {
+		const newsContainer = document.querySelector('.allNews');
+		// Iterate through items from JSON file in reverse
+		for (let i = (news.length - 1); i >= 0; i--) {
+			// Display all found items
+			displayNews(news[i], newsContainer);
+		} 
+	} else if (document.querySelector('.latestNews')) {
+		let latestNews = [];
+		const newsContainer = document.querySelector('.latestNews');
+
+		// Pushing the two latest items into latestNews array
+		latestNews.push(news.pop());
+		latestNews.push(news.pop());
+
+		for (let i = 0; i < latestNews.length; i++) {
+			// Display all found items
+			displayNews(latestNews[i], newsContainer);
+		} 
+	}
+	
 }
 
 // Display all news items given to the function
-function displayNews(newsItem) {
-	const newsContainer = document.querySelector('.section-content'),
-		newsWrapper = document.createElement('div'),
+function displayNews(newsItem, newsContainer) {
+	const newsWrapper = document.createElement('div'),
 		newsArticle = document.createElement('div'),
-		anchorElement = document.createElement('a');
+		anchorElement = document.createElement('a'),
+		date = (newsItem.date.getMonth() + 1) + "/" + newsItem.date.getDate() + "/" + newsItem.date.getFullYear();
 
 	// Assign attributes and HTML to elements
 	newsArticle.className = "news-article";
 	anchorElement.setAttribute("href", "news.html");
 	anchorElement.innerHTML = `
+		<div class="date">${date}</div>
 		<img src="${newsItem.imageSrc}" alt="${newsItem.imageAlt}">
 		<h3>${newsItem.title}</h3>
 		<p>${newsItem.shortText}</p>
@@ -26,8 +50,6 @@ function displayNews(newsItem) {
 	newsArticle.appendChild(anchorElement);
 	newsWrapper.appendChild(newsArticle);
 	newsContainer.appendChild(newsWrapper);
-
-
 }
 
 // Get news from json
