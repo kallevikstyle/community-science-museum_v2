a102c2c3588b3c8295f29cf951dc472df0bafb03
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-11-24 14:56:06 +0100

    Adding mouseover event to give buttons a nudge when hovering the eye button

--- scripts/circular-menu.js
+++ scripts/circular-menu.js
@@ -1,8 +1,16 @@
 
 const eyeButton = document.querySelector('#eye-button');
 
-
+// Toggle .open class to spread out the exhibition buttons
 eyeButton.addEventListener("click", function(e) {
 	e.preventDefault();
 	document.querySelector('#circular-buttons').classList.toggle('open');
+});
+
+// Mouseover event on eye button to give the hidden buttons a nudge
+eyeButton.addEventListener("mouseover", function(e) {
+	document.querySelector('#circular-buttons').classList.toggle('nudge');
+	setTimeout(function() {
+		document.querySelector('#circular-buttons').classList.toggle('nudge');
+	}, 250);
 });
